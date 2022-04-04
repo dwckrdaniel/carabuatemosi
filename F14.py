8659573d645ec68a0595bc0fb3767dc76cf35dbd
dwckrdaniel <102867554+dwckrdaniel@users.noreply.github.com>
2022-04-04 22:47:42 +0700

    Create F14.py

---
+++ F14.py
@@ -0,0 +1,33 @@
+# KAMUS
+# adminORuser : string
+
+def help(adminORuser): # F16 - Help
+    if adminORuser == "admin":
+        print('''
+        ============HELP============
+        register - untuk melakukan registrasi user baru
+        login - untuk melakukan login ke dalam sistem
+        tambah_game - Untuk menambah game yang dijual pada toko
+        ubah_game - mengubah game yang ad di toko
+        ubah_stok - mengubah stok game yang ad di toko
+        list_game_toko - Untuk melihat list game yang dijual pada toko
+        search_game_at_store - melakukan pencarian game di toko
+        topup - menambahkan saldo kepada user
+        help - untuk memberikan panduan penggunaan sistem
+        save - untuk menyimpan data
+        exit - untuk keluar dari aplikasi
+        ''')
+    else:
+        print('''
+        ---HELP---
+        login - untuk melakukan login ke dalam sistem
+        list_game_toko - Untuk melihat list game yang dijual pada toko
+        buy_game - membeli game yang terdapat di toko
+        list_game - melihat daftar game yang dimiliki pengguna
+        search_my_game - memberikan informasi game yang di dalam inventory user
+        search_game_at_store - melakukan pencarian game di toko
+        riwayat - melihat riwayat pembelian game
+        help - untuk memberikan panduan penggunaan sistem
+        save - untuk menyimpan data
+        exit - untuk keluar dari aplikasi
+        ''')
