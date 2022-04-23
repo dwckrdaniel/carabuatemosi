b02e085f52240aa24c3d13e267f791281e147d51
dwckrdaniel <102867554+dwckrdaniel@users.noreply.github.com>
2022-04-23 17:23:28 +0700

    Update F15.py

--- F15.py
+++ F15.py
@@ -21,11 +21,11 @@ def load1(dir):
       temp = ""
       for words in x:
           if words == delimitter:
-              hasil = [temp]+hasil
+              hasil = hasil+[temp]
               temp = ""
           else:
               temp += words
-      hasil= [temp]+hasil
+      hasil= hasil + [temp]
       return hasil
 
   def convert_line_to_data(line):
@@ -33,26 +33,33 @@ def load1(dir):
     array_of_data = [data.strip() for data in raw_array_of_data]
     return array_of_data
   
-  def my_pop(l):
+  def my_pop(lines1,raw_header):
     new = []
-    l = list(l)
-    for i in l:
-        if l[-1] == i:
+    raw_header = lines1[0]
+    for rows in lines1 :
+        if lines1[0] == rows:
             pass
         else:
-            new = [i]+new
+            new = new+[rows]
     return new
-  raw_header1 = lines1.my_pop(0)
+  raw_header1 = []
+  lines1 = my_pop(lines1,raw_header1)
 
   header1 = convert_line_to_data(raw_header1)
 
   datas1 = []
+  
+  def findLength(string):
+      count = 0
+      for i in string:
+          count+= 1
+      return count
 
-  for line in lines1:
-    array_of_data = convert_line_to_data(line)
+  for i in range(findLength(lines1)):
+    array_of_data = convert_line_to_data(lines1[i])
     real_values1 = convert_array_data_to_real_values1(array_of_data)
-    datas1 = [real_values1] + datas1
-  return [header1,datas1]
+    datas1 = datas1 +[real_values1]
+  return header1,datas1
 
 def load2(dir):
   dir = "./"+dir+"/game.csv"
@@ -61,7 +68,7 @@ def load2(dir):
   f2.close()
 
   lines2 = [raw_line.replace("\n", "") for raw_line in raw_lines2] # hapus spasi
-
+  
   def convert_array_data_to_real_values2(array_data):
     arr_cpy = array_data[:]
     for i in range(5):    # ubah id ke integer           
@@ -76,11 +83,11 @@ def load2(dir):
       temp = ""
       for words in x:
           if words == delimitter:
-              hasil = [temp]+hasil
+              hasil = hasil+[temp]
               temp = ""
           else:
               temp += words
-      hasil= [temp]+hasil
+      hasil= hasil + [temp]
       return hasil
     
   def convert_line_to_data(line):
@@ -88,26 +95,33 @@ def load2(dir):
     array_of_data = [data.strip() for data in raw_array_of_data]
     return array_of_data
   
-  def my_pop(l):
+  def my_pop(lines2,raw_header):
     new = []
-    l = list(l)
-    for i in l:
-        if l[-1] == i:
+    raw_header=lines2[0]
+    for rows in lines2:
+        if lines2[0] == rows:
             pass
         else:
-            new = [i]+new
+            new = new + [rows]
     return new
-  raw_header2 = lines2.my_pop(0)
+  raw_header2 = []
+  lines2 = my_pop(lines2,raw_header2)
 
   header2 = convert_line_to_data(raw_header2)
 
   datas2 = []
+  
+  def findLength(string):
+      count = 0
+      for i in string:
+          count+= 1
+      return count 
 
-  for line in lines2:
-    array_of_data = convert_line_to_data(line)
+  for i in range(findLength(lines2)):
+    array_of_data = convert_line_to_data(lines2[i])
     real_values2 = convert_array_data_to_real_values2(array_of_data)
-    datas2=[real_values2]+datas2
-  return [header2,datas2]
+    datas2 = datas2 +[real_values2]
+  return header2,datas2
 
 def load3(dir):
   dir = "./"+dir+"/riwayat.csv"
@@ -124,7 +138,6 @@ def load3(dir):
         arr_cpy[i] = int(arr_cpy[i])
       if(i == 3): 
         arr_cpy[i] = int(arr_cpy[i])
-
     return arr_cpy
 
   def split(x, delimitter):
@@ -132,38 +145,45 @@ def load3(dir):
       temp = ""
       for words in x:
           if words == delimitter:
-              hasil = [temp]+hasil
+              hasil = hasil+[temp]
               temp = ""
           else:
               temp += words
-      hasil= [temp]+hasil
+      hasil= hasil + [temp]
       return hasil
-
+    
   def convert_line_to_data(line):
     raw_array_of_data = split(line,";") # ubah string jadi integer
     array_of_data = [data.strip() for data in raw_array_of_data]
     return array_of_data
   
-  def my_pop(l):
+  def my_pop(lines3,raw_header):
     new = []
-    l = list(l)
-    for i in l:
-        if l[-1] == i:
+    raw_header=lines3[0]
+    for rows in lines3:
+        if lines3[0] == rows:
             pass
         else:
-            new = [i]+new
+            new = new + [rows]
     return new
-  raw_header3 = lines3.my_pop(0)
+  raw_header3 = []
+  lines3 = my_pop(lines3,raw_header3)
 
   header3 = convert_line_to_data(raw_header3)
 
   datas3 = []
+  
+  def findLength(string):
+      count = 0
+      for i in string:
+          count+= 1
+      return count 
 
-  for line in lines3:
-    array_of_data = convert_line_to_data(line)
+  for i in range(findLength(lines3)):
+    array_of_data = convert_line_to_data(lines3[i])
     real_values3 = convert_array_data_to_real_values3(array_of_data)
-    datas3=[real_values3]+datas3
-  return [header3,datas3]
+    datas3 = datas3 +[real_values3]
+  return header3,datas3
 
 def load4(dir):
   dir = "./"+dir+"/kepemilikan.csv"
@@ -185,11 +205,11 @@ def load4(dir):
       temp = ""
       for words in x:
           if words == delimitter:
-              hasil = [temp]+hasil
+              hasil = hasil+[temp]
               temp = ""
           else:
               temp += words
-      hasil= [temp]+hasil
+      hasil= hasil + [temp]
       return hasil
 
   def convert_line_to_data(line):
@@ -197,23 +217,30 @@ def load4(dir):
     array_of_data = [data.strip() for data in raw_array_of_data]
     return array_of_data
   
-  def my_pop(l):
+  def my_pop(lines4,raw_header):
     new = []
-    l = list(l)
-    for i in l:
-        if l[-1] == i:
+    raw_header=lines4[0]
+    for rows in lines4:
+        if lines4[0] == rows:
             pass
         else:
-            new = [i]+new
+            new = new + [rows]
     return new
-  raw_header4 = lines4.my_pop(0)
+  raw_header4 = []
+  lines4 = my_pop(lines4,raw_header4)
 
   header4 = convert_line_to_data(raw_header4)
 
   datas4 = []
+  
+  def findLength(string):
+      count = 0
+      for i in string:
+          count+= 1
+      return count 
 
-  for line in lines4:
-    array_of_data = convert_line_to_data(line)
+  for i in range(findLength(lines4)):
+    array_of_data = convert_line_to_data(lines4[i])
     real_values4 = convert_array_data_to_real_values4(array_of_data)
-    datas4= [real_values4]+datas4
-  return [header4,datas4]
+    datas4 = datas4 +[real_values4]
+  return header4,datas4
